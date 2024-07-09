cb0b64ed4214ed64500e94441b209b706b5eea7e
dikahariss <dika.webmaster@gmail.com>
2024-07-09 15:33:53 +0700

    initial hello

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World from Node.js on OpenShift!');
+});
+
+const port = 8080;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
